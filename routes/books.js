6f2a1acb71c6f548c9df349bac23c2b8f40af4f8
GrzegorzZbagien <haribopoland@gmail.com>
2022-09-26 22:59:34 +0200

    books 1 from 2

---
+++ routes/books.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router = express.Router()
+const Book = require('../models/book')
+
+
+// All Book Route
+router.get('/', async (req,res) => {
+res.send('All Books')
+})
+
+// New Book Route
+router.get('/new', (req, res) => {
+    res.send('New Book')
+})
+
+// Create Book Route
+router.post('/', async (req,res) => {
+    res.send('Create Book')
+ })
+
+
+
+module.exports = router
